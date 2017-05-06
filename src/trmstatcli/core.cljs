1a5ac2a54e331f3d5435320f8e7a66b349e22fd9
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2017-05-06 12:51:56 +0200

    Trying to use node-https

--- src/trmstatcli/core.cljs
+++ src/trmstatcli/core.cljs
@@ -4,8 +4,11 @@
             [httpurr.client.node :refer [client]]
             [archiet.trm.stac.core :as st]
             [promesa.core :as p :refer [await] :refer-macros [alet]]))
+(def h (nodejs/require "https"))
+
 (defn -main []
-  (alet [blah (p/await (http/get client st/url))] (println (-> blah :body str ))))
+  ;;(alet [blah (p/await (http/get client st/url))] (println (-> blah :body str )))
+  (.get h st/url #(println (.read %))))
 
 (nodejs/enable-util-print!)
 
